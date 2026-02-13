ae558350f2be80af1aca467c6d21fdbffd17f032
rei <raihanryuichi@gmail.com>
2026-02-13 13:10:50 +0700

    refactor: tenant table as component

---
+++ app/components/Table/Tenant.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { h, resolveComponent } from 'vue'
+import type { TableColumn } from '@nuxt/ui'
+
+defineProps<{
+    data: Tenant[]
+    total: number
+    pending: boolean
+}>()
+const limit = defineModel<number>('limit', { default: 0 })
+const page = defineModel<number>('page', { default: 0 })
+
+function useColumns() {
+    const UBadge = resolveComponent('UBadge')
+    const UButton = resolveComponent('UButton')
+    const columns: TableColumn<Tenant>[] = [
+        {
+            accessorKey: 'name',
+            header: 'Tenant Name',
+            cell: ({ row }) => {
+                return h('div', {}, [
+                    h('span', { class: 'font-semibold' }, row.original.name),
+                    h('br'),
+                    h('span', { class: 'text-sm' }, row.original.owner?.name || ''),
+                ])
+            },
+        },
+        {
+            accessorKey: 'owner.email',
+            header: 'Admin Email',
+            cell: ({ row }) => {
+                return h('div', {}, [
+                    h('span', {}, row.original.owner?.email || ''),
+                    h('br'),
+                    h('span', { class: 'text-sm' }, row.original.owner?.phone?.number || ''),
+                ])
+            },
+        },
+        {
+            accessorKey: 'plan',
+            header: 'Plan',
+            cell: ({ row }) => {
+                return h(UBadge, {
+                    color: 'secondary',
+                    variant: 'subtle',
+                    label: row.original.plan,
+                })
+            },
+        },
+        {
+            accessorKey: 'total_event',
+            header: 'Events',
+        },
+        {
+            accessorKey: 'status',
+            header: 'Status',
+            cell: ({ row }) => {
+                return h(UBadge, {
+                    color: STATUS_COLORS[row.getValue('status') as Status],
+                    variant: 'subtle',
+                    label: row.original.status,
+                })
+            },
+        },
+        {
+            accessorKey: 'tenant_id',
+            header: 'Action',
+            cell: ({ row }) => {
+                return h('div', { class: 'flex gap-2' }, [
+                    h(UButton, {
+                        color: 'secondary',
+                        variant: 'ghost',
+                        icon: 'lucide:pencil',
+                        to: `tenants/${row.original.tenant_id}/edit`,
+                    }),
+                    h(UButton, {
+                        color: 'secondary',
+                        variant: 'ghost',
+                        icon: 'lucide:info',
+                        to: `tenants/${row.original.tenant_id}`,
+                    }),
+                ])
+            },
+        },
+    ]
+    return columns
+}
+
+const columns = useColumns()
+</script>
+
+<template>
+    <div>
+        <UTable
+            :data="data"
+            :columns="columns"
+            :loading="pending"
+        />
+
+        <MiscPagination
+            v-model:limit="limit"
+            v-model:page="page"
+            :total="total"
+        />
+    </div>
+</template>
--- app/pages/tenants/index.vue
+++ app/pages/tenants/index.vue
@@ -1,87 +1,7 @@
 <script setup lang="ts">
-import { h, resolveComponent } from 'vue'
-import type { TableColumn } from '@nuxt/ui'
-
 const route = useRoute()
 
-function useColumns() {
-    const UBadge = resolveComponent('UBadge')
-    const UButton = resolveComponent('UButton')
-    const columns: TableColumn<Tenant>[] = [
-        {
-            accessorKey: 'name',
-            header: 'Tenant Name',
-            cell: ({ row }) => {
-                return h('div', {}, [
-                    h('span', { class: 'font-semibold' }, row.original.name),
-                    h('br'),
-                    h('span', { class: 'text-sm' }, row.original.owner?.name || ''),
-                ])
-            },
-        },
-        {
-            accessorKey: 'owner.email',
-            header: 'Admin Email',
-            cell: ({ row }) => {
-                return h('div', {}, [
-                    h('span', {}, row.original.owner?.email || ''),
-                    h('br'),
-                    h('span', { class: 'text-sm' }, row.original.owner?.phone?.number || ''),
-                ])
-            },
-        },
-        {
-            accessorKey: 'plan',
-            header: 'Plan',
-            cell: ({ row }) => {
-                return h(UBadge, {
-                    color: 'secondary',
-                    variant: 'subtle',
-                    label: row.original.plan,
-                })
-            },
-        },
-        {
-            accessorKey: 'total_event',
-            header: 'Events',
-        },
-        {
-            accessorKey: 'status',
-            header: 'Status',
-            cell: ({ row }) => {
-                return h(UBadge, {
-                    color: STATUS_COLORS[row.getValue('status') as Status],
-                    variant: 'subtle',
-                    label: row.original.status,
-                })
-            },
-        },
-        {
-            accessorKey: 'tenant_id',
-            header: 'Action',
-            cell: ({ row }) => {
-                return h('div', { class: 'flex gap-2' }, [
-                    h(UButton, {
-                        color: 'secondary',
-                        variant: 'ghost',
-                        icon: 'lucide:pencil',
-                        to: `tenants/${row.original.tenant_id}/edit`,
-                    }),
-                    h(UButton, {
-                        color: 'secondary',
-                        variant: 'ghost',
-                        icon: 'lucide:info',
-                        to: `tenants/${row.original.tenant_id}`,
-                    }),
-                ])
-            },
-        },
-    ]
-    return columns
-}
-
 async function useList() {
-    const columns = useColumns()
     const search = ref('')
     const query = ref('')
     const page = ref(1)
@@ -109,7 +29,6 @@ async function useList() {
     }
 
     return {
-        columns,
         search,
         page,
         limit,
@@ -122,7 +41,6 @@ async function useList() {
 }
 
 const {
-    columns,
     search,
     page,
     limit,
@@ -170,19 +88,14 @@ setLayoutPropState(buildLayoutProp(APP_ROUTES, route.path, {}))
                     </div>
                 </div>
             </template>
-            <div>
-                <UTable
-                    :data="tenants"
-                    :columns="columns"
-                    :loading="pending"
-                />
 
-                <MiscPagination
-                    v-model:limit="limit"
-                    v-model:page="page"
-                    :total="total"
-                />
-            </div>
+            <TableTenant
+                v-model:limit="limit"
+                v-model:page="page"
+                :data="tenants"
+                :total="total"
+                :pending="pending"
+            />
         </UCard>
     </div>
 </template>
