767da7bfe0e23d943ba9845fb76b504d4f7aa9b2
rei <raihanryuichi@gmail.com>
2026-02-16 10:02:36 +0700

    remove dummy data

--- shared/types/data.d.ts
+++ shared/types/data.d.ts
@@ -9,25 +9,6 @@ export type ISOString = string
 
 export type RoleSlug = 'superadmin' | 'tenant.admin' | 'tenant.poc'
 
-export interface DummyTenant {
-    id?: number
-    tenantName: string
-    status: Status
-
-    adminName?: string
-    adminEmail?: string
-    adminPhone?: string
-    adminPassword?: string
-    planId?: number
-    billingAddress?: string
-    createdAt?: string
-
-    confirmPassword?: string
-    plan?: string
-    events?: number
-    registeredUsers?: number
-}
-
 export interface TenantOwnerPhone {
     number: string
     country_code?: string
