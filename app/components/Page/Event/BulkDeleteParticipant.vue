924738196046e3ea748ad38d44d91acd9c3f635a
rei <raihanryuichi@gmail.com>
2026-04-13 10:00:33 +0700

    feat: bulk delete participant

---
+++ app/components/Page/Event/BulkDeleteParticipant.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const props = defineProps<{
+    tenantId: number
+    eventId: number
+    selectedIds: number[]
+    hide?: boolean
+}>()
+const emit = defineEmits([EMIT_DETAIL_REFRESH])
+const bulkDeleteConfirmation = defineModel<boolean>('open', { default: false })
+
+const { $api } = useNuxtApp()
+const toast = useToast()
+const bulkDeleteLoading = ref(false)
+
+async function bulkDelete() {
+    const ids = props.selectedIds.length > 0 ? props.selectedIds : []
+    try {
+        bulkDeleteLoading.value = true
+        await $api(`/api/tenant/${props.tenantId}/event/${props.eventId}/participant/bulk/hard`, {
+            method: 'DELETE',
+            body: ids.length ? { participant_ids: ids } : {},
+        })
+    }
+    catch (error) {
+        toast.add({
+            title: 'Error',
+            description: 'Failed to bulk delete',
+            color: 'error',
+        })
+        console.error('Bulk delete error', error)
+    }
+    finally {
+        bulkDeleteConfirmation.value = false
+        bulkDeleteLoading.value = false
+        emit(EMIT_DETAIL_REFRESH)
+    }
+}
+</script>
+
+<template>
+    <UButton
+        v-if="!hide"
+        color="neutral"
+        variant="outline"
+        icon="lucide:qr-code"
+        class="cursor-pointer"
+        @click="bulkDeleteConfirmation = true"
+    >
+        {{ `Bulk Delete ${selectedIds.length ? `(${selectedIds.length})` : ''}` }}
+    </UButton>
+
+    <ModalConfirmNegativeAction
+        v-model:open="bulkDeleteConfirmation"
+        title="Bulk Delete Participant Confirmation"
+        :body="`You will delete ${selectedIds.length || 'All'} participants, Continue?`"
+        :confirm-label="`Yes, Delete ${selectedIds.length || 'All'} Participants`"
+        :loading="bulkDeleteLoading"
+        @confirm="bulkDelete"
+    />
+</template>
---
+++ server/api/tenant/[tenant_id]/event/[event_id]/participant/bulk/hard.delete.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(async (event): Promise<FetchResult> => {
+    const method = 'DELETE'
+    const tenantId = getRouterParam(event, 'tenant_id')
+    const eventId = getRouterParam(event, 'event_id')
+    const path = `/tenant/${tenantId}/event/${eventId}/participant/bulk/hard`
+    const body = await readBody(event)
+
+    const res: FetchResult = await api(event, method, path, {
+        body,
+    })
+    if (res.success) {
+        return res
+    }
+    else {
+        console.error(`${method} ${path} failed`, res)
+        return res
+    }
+})
