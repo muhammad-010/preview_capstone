ebf5601ceb95c5133e6c4ba6b1a1bc0e8a1a0b13
rei <raihanryuichi@gmail.com>
2026-04-13 10:46:09 +0700

    feat: new check-in api, deprecation list

---
+++ server/api/tenant/[tenant_id]/event/[event_id]/session/[session_id]/check-in/confirm/manual.post.ts
@@ -0,0 +1,19 @@
+export default defineEventHandler(async (event): Promise<FetchResult> => {
+    const method = 'POST'
+    const tenantId = getRouterParam(event, 'tenant_id')
+    const eventId = getRouterParam(event, 'event_id')
+    const sessionId = getRouterParam(event, 'session_id')
+    const path = `/tenant/${tenantId}/event/${eventId}/session/${sessionId}/check-in/confirm/manual`
+    const body = await readBody(event)
+
+    const res: FetchResult = await api(event, method, path, {
+        body,
+    })
+    if (res.success) {
+        return res
+    }
+    else {
+        console.error(`${method} ${path} failed`, res)
+        return res
+    }
+})
