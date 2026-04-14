8c3d72f9e53ee8901dbfa04978fc6f16bced7701
rei <raihanryuichi@gmail.com>
2026-04-14 15:31:35 +0700

    remove console.log

--- server/utils/api.methods.ts
+++ server/utils/api.methods.ts
@@ -29,7 +29,6 @@ async function _api<T>(
     }
     return await $fetch.create({
         onResponseError({ response }) {
-            console.log(response)
             console.error({
                 url: response.url,
                 status: response.status,
