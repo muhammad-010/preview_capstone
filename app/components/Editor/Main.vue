0db44416e650e4d4706d6b9d2632a8d8a22dc161
rei <raihanryuichi@gmail.com>
2026-04-27 17:03:27 +0700

    fix: jpg limit on editor

--- app/components/Editor/Main.vue
+++ app/components/Editor/Main.vue
@@ -860,7 +860,7 @@ function preview() {
                             <UFileUpload
                                 v-slot="{ open, removeFile }"
                                 v-model="bgImage.perBreakpoint![activeCanvasSizeId]!.file"
-                                accept="image/jpg"
+                                accept=".jpg, .jpeg, image/jpeg"
                             >
                                 <UFieldGroup>
                                     <UInput
