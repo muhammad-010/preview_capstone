a4e2be932f8e196c934826d860023695b63cac67
rei <raihanryuichi@gmail.com>
2026-07-22 14:20:42 +0700

    feat: modal detail use accordion

--- app/components/Page/Order/ModalDetail.vue
+++ app/components/Page/Order/ModalDetail.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
 const open = defineModel<boolean>('open', { default: false })
 
+const sections = [
+    {
+        label: 'ORDER INFORMATION',
+        slot: 'order-information',
+    },
+    {
+        label: 'PURCHASED ITEMS',
+        slot: 'purchased-items',
+    },
+    {
+        label: 'TICKETS OWNER',
+        slot: 'tickets-owner',
+    },
+]
+const activeSection = ref(['0'])
+watch(open, (newData) => {
+    if (!newData) {
+        activeSection.value = ['0']
+    }
+})
+
 function formatCurrency(value: number, currency?: string): string {
     const formatter = getCurrencyFormatter(currency)
     return formatter.format(value)
@@ -55,176 +76,155 @@ function formatCurrency(value: number, currency?: string): string {
                 </div>
             </section>
 
-            <section class="border-b border-default pb-4 mb-4">
-                <div class="col-span-2 text-sm font-bold text-toned mb-3">
-                    CUSTOMER INFORMATION
-                </div>
-
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="text-sm">
-                        Name
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        Ramona
-                    </div>
-
-                    <div class="text-sm">
-                        Email
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        ramona@mythag.com
-                    </div>
-                </div>
+            <section class="flex flex-col justify-center items-center gap-2 border-b border-default pb-4 mb-4">
+                <span class="text-sm text-toned">Total Amount</span>
+                <span class="text-4xl font-bold">{{ formatCurrency(1500000) }}</span>
             </section>
 
-            <section class="border-b border-default pb-4 mb-4">
-                <div class="col-span-2 text-sm font-bold text-toned mb-3">
-                    PURCHASED ITEMS
-                </div>
+            <UAccordion
+                v-model="activeSection"
+                type="multiple"
+                :items="sections"
+            >
+                <template #order-information>
+                    <div class="grid grid-cols-2 gap-2 mb-4">
+                        <div class="text-sm">
+                            Customer Name
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Ramona
+                        </div>
 
-                <div class="grid grid-cols-2 gap-4 not-last:mb-4 not-last:pb-4 not-last:border-b not-last:border-default">
-                    <div class="col-span-2 flex justify-between items-center text-sm">
-                        <p class="font-semibold">
-                            Ticket One
-                        </p>
+                        <div class="text-sm">
+                            Customer Email
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            ramona@mythag.com
+                        </div>
 
-                        <UCollapsible>
-                            <template #default="{ open }">
-                                <UButton
-                                    :label="`${open ? 'Hide' : 'Show'} Owner (${3})`"
-                                    color="neutral"
-                                    size="sm"
-                                    variant="subtle"
-                                    :trailing-icon="`i-lucide-chevron-${open ? 'up' : 'down'}`"
-                                />
-                            </template>
+                        <div class="text-sm">
+                            Payment Method
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Virtual Account (BCA VA)
+                        </div>
 
-                            <template #content>
-                                <div class="text-right text-xs font-semibold">
-                                    <p class="my-1">
-                                        Ramona
-                                    </p>
-                                    <p class="my-1">
-                                        Thais
-                                    </p>
-                                    <p class="my-1">
-                                        Corposant
-                                    </p>
-                                </div>
-                            </template>
-                        </UCollapsible>
+                        <div class="text-sm">
+                            Paid Amount
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatCurrency(1500000) }}
+                        </div>
+
+                        <div class="text-sm">
+                            Ordered At
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatLongDate('2026-07-06T17:16:32+07:00') }}
+                        </div>
+
+                        <div class="text-sm">
+                            Paid At
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatLongDate('2026-07-06T17:16:32+07:00') }}
+                        </div>
+
+                        <div class="text-sm">
+                            Purchase Platform
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Rawooh Tickets
+                        </div>
                     </div>
+                </template>
 
-                    <div class="text-sm">
-                        <div class="text-xs text-toned flex justify-between items-end w-[70%]">
-                            <span>3 x</span>
+                <template #purchased-items>
+                    <div class="grid grid-cols-2 gap-4 not-last:mb-4 not-last:pb-4 not-last:border-b not-last:border-default">
+                        <div class="col-span-2 flex justify-between items-center text-sm">
+                            <p class="font-semibold">
+                                Ticket One
+                            </p>
+                        </div>
 
-                            <div class="text-right flex flex-col">
-                                <div class="flex items-center justify-between gap-4">
-                                    <p class="line-through">
-                                        {{ formatCurrency(1000000) }}
-                                    </p>
-                                    <UBadge
-                                        color="warning"
-                                        variant="subtle"
-                                        size="sm"
-                                        label="50% Off"
-                                    />
-                                </div>
+                        <div class="text-sm">
+                            <div class="text-xs text-toned flex justify-between items-end w-[70%]">
+                                <span>3 x</span>
 
-                                <div class="flex items-center justify-between gap-4">
-                                    <p>{{ formatCurrency(500000) }}</p>
+                                <div class="text-right flex flex-col">
+                                    <div class="flex items-center justify-between gap-4">
+                                        <p class="line-through">
+                                            {{ formatCurrency(1000000) }}
+                                        </p>
+                                        <UBadge
+                                            color="warning"
+                                            variant="subtle"
+                                            size="sm"
+                                            label="50% Off"
+                                        />
+                                    </div>
+
+                                    <div class="flex items-center justify-between gap-4">
+                                        <p>{{ formatCurrency(500000) }}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="text-sm text-right font-bold flex flex-col justify-end">
+                            {{ formatCurrency(1500000) }}
+                        </div>
                     </div>
 
-                    <div class="text-sm text-right font-bold flex flex-col justify-end">
-                        {{ formatCurrency(1500000) }}
-                    </div>
-                </div>
-            </section>
+                    <div class="grid grid-cols-2 gap-2 mb-4">
+                        <div class="text-sm">
+                            Subtotal
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatCurrency(1500000) }}
+                        </div>
 
-            <section class="border-b border-default pb-4 mb-4">
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="text-sm">
-                        Subtotal
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        {{ formatCurrency(1500000) }}
-                    </div>
+                        <div class="text-sm">
+                            Taxes & Fees
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatCurrency(0) }}
+                        </div>
 
-                    <div class="text-sm">
-                        Taxes & Fees
+                        <div class="text-sm">
+                            Total Amount
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            {{ formatCurrency(1500000) }}
+                        </div>
                     </div>
-                    <div class="text-sm text-right font-bold">
-                        {{ formatCurrency(0) }}
-                    </div>
+                </template>
 
-                    <div class="text-2xl font-bold">
-                        Total Amount
-                    </div>
-                    <div class="text-2xl text-right font-bold">
-                        {{ formatCurrency(1500000) }}
-                    </div>
-                </div>
-            </section>
+                <template #tickets-owner>
+                    <div class="grid grid-cols-2 gap-2">
+                        <div class="text-sm">
+                            Ramona
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Ticket One
+                        </div>
 
-            <!-- <section class="flex flex-col justify-center items-center gap-2 border-b border-default pb-4 mb-4"> -->
-            <!--   <span class="text-sm text-toned">Total Amount</span> -->
-            <!--   <span class="text-4xl font-bold">{{ formatCurrency(1500000) }}</span> -->
-            <!-- </section> -->
+                        <div class="text-sm">
+                            Thais
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Ticket One
+                        </div>
 
-            <section class="border-b border-default pb-4 mb-4">
-                <div class="col-span-2 text-sm font-bold text-toned mb-3">
-                    PAYMENT INFORMATION
-                </div>
-
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="text-sm">
-                        Payment Method
+                        <div class="text-sm">
+                            Miryam
+                        </div>
+                        <div class="text-sm text-right font-bold">
+                            Ticket One
+                        </div>
                     </div>
-                    <div class="text-sm text-right font-bold">
-                        Virtual Account (BCA VA)
-                    </div>
-
-                    <div class="text-sm">
-                        Paid Amount
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        {{ formatCurrency(1500000) }}
-                    </div>
-                </div>
-            </section>
-
-            <section class="my-4">
-                <div class="text-sm font-bold text-toned mb-4">
-                    ORDER INFORMATION
-                </div>
-
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="text-sm">
-                        Ordered At
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        {{ formatLongDate('2026-07-06T17:16:32+07:00') }}
-                    </div>
-
-                    <div class="text-sm">
-                        Paid At
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        {{ formatLongDate('2026-07-06T17:16:32+07:00') }}
-                    </div>
-
-                    <div class="text-sm">
-                        Purchase Platform
-                    </div>
-                    <div class="text-sm text-right font-bold">
-                        Rawooh Tickets
-                    </div>
-                </div>
-            </section>
+                </template>
+            </UAccordion>
         </template>
     </UModal>
 </template>
