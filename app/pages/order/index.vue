2f8b94be3ef97d50fd79d790ff32c2aafdec2985
rei <raihanryuichi@gmail.com>
2026-07-28 13:27:07 +0700

    fix: order tenant-id

--- app/pages/order/index.vue
+++ app/pages/order/index.vue
@@ -190,6 +190,7 @@ setLayoutPropState(buildLayoutProp(APP_ROUTES, route.path, {}))
             <PageOrderTable
                 v-model:limit="limit"
                 v-model:page="page"
+                :tenant-id="tenantId"
                 :data="list"
                 :total="total"
                 :pending="pending"
